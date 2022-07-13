07d0f49daefea85962fe436ab671da8791488484
seth-z-fry <sethzfry@icloud.com>
2022-07-12 21:42:50 -0500

    Template and URLs created

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from turtle import title
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class PublishedManager(models.Manager):
@@ -30,6 +31,12 @@ class Post (models.Model):
     
     def __str__(self):
         return self.title
+
+    def get_absoulte_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year,
+                              self.publish.month,
+                              self.publish.day, self.slug])
 
 
 
